f7c7e5e211a133803ac6fd0bd1e28ba7a3e39390
legojinK <ckgksck@gmail.com>
2022-05-13 21:58:38 +0900

    [김희진] 날씨 아이콘 추가 [FWMS]

--- HeejinKim/frontend/frontend_pro/src/components/main/Weather.vue
+++ HeejinKim/frontend/frontend_pro/src/components/main/Weather.vue
@@ -1,54 +1,69 @@
 <template>
+  <v-container class="wee" >
+   
+      
+        <v-card class="color">
+    
+        <v-card-title class="text-center justify-center py-6">
+          <h1 class="font-weight-bold grey--text"> Weather </h1>
+        </v-card-title>  
 
-  <div id="app" :class="typeof weather.main != 'undefined' && this.temp > 15 ?'warm' : ''">
-    <v-card>
-      <v-card-title class="text-center justify-center py-6">
-        <h1 class="font-weight-bold grey--text"> Weather </h1>
-      </v-card-title>  
+        <v-card-title class="text-right justify-center ">
+          <h5 class="date"> {{ dateBuilder() }}</h5>
+        </v-card-title>
 
-      <v-card-title class="text-right justify-center ">
-        <h5 class="date"> {{ dateBuilder() }}</h5>
-      </v-card-title>
+        <v-tabs v-model="tab" color="grey" grow>
+          <v-tab v-for="city in cities" :key="city" @click="selectCity(city)">
+            {{ city }}
+          </v-tab>
+        </v-tabs>
 
-      <v-tabs v-model="tab" background-color="transparent" color="grey" grow>
-        <v-tab v-for="city in cities" :key="city" @click="selectCity(city)">
-          {{ city }}
-        </v-tab>
-      </v-tabs>
+        <v-tabs-items v-model="tab">
+          <v-tab-item v-for="city in cities" :key="city" >
+            <v-card  class="color">
+              <v-card-title class="headline gray--text font-weight-bold justify-center ma-5">
+                {{ city }}
+              </v-card-title>
 
-      <v-tabs-items v-model="tab">
-        <v-tab-item v-for="city in cities" :key="city" >
-          <v-card class="white">
-            <v-card-title class="headline gray--text font-weight-bold justify-center ma-5">
-              {{ city }}
-            </v-card-title>
+              <v-list class="color">  
+                <v-list-item class="text-center">
+                  <v-list-item-title class="title font-weight-bold gray--text">
+                  
+                <v-img v-if="`${icon}`=='01d'" class= "sun" :src="require(`@/assets/icon/${icon}.png`)"></v-img>
+                <v-img v-if="`${icon}`!='01d'" class= "nosun" :src="require(`@/assets/icon/${icon}.png`)"></v-img>
+                  </v-list-item-title>
+                </v-list-item> 
+                <v-list-item class="text-center">
+                  <v-list-item-title class="title font-weight-bold gray--text">
+                    {{ weather }}
+                  </v-list-item-title>
+                </v-list-item> 
 
-            <v-list>  
-              <v-list-item class="text-center">
-                <v-list-item-title class="title font-weight-bold gray--text">
-                  {{ weather }}
-                </v-list-item-title>
-              </v-list-item> 
+                <v-list-item class="text-center">
+                  <v-list-item-title class="gray--text">
+                    <v-icon>mdi-thermometer</v-icon>
+                    {{ temp }} ℃
+                  </v-list-item-title>
+                </v-list-item>
 
-              <v-list-item class="text-center">
-                <v-list-item-title class="gray--text">
-                  {{ temp }} ℃
-                </v-list-item-title>
-              </v-list-item>
+                <v-list-item class="text-center">
+                  <v-list-item-title class="gray--text">
+                    <v-icon>mdi-water-percent</v-icon>
+                    {{ humidity }} %
+                  </v-list-item-title>
+                </v-list-item>  
 
-              <v-list-item class="text-center">
-                <v-list-item-title class="gray--text">
-                  {{ humidity }} %
-                </v-list-item-title>
-              </v-list-item>  
+              </v-list>
+            </v-card>
+          </v-tab-item>
+        </v-tabs-items> 
+      </v-card>
+ 
+  
+    
+ 
 
-            </v-list>
-          </v-card>
-        </v-tab-item>
-      </v-tabs-items>
-
-    </v-card>
-  </div>
+</v-container>
 </template>
 
 
@@ -65,6 +80,8 @@ export default {
       weather:'' ,
       tab: null,
       city: 'Seoul',
+      icon: '02d',
+     
       
       
       cities: [
@@ -104,13 +121,20 @@ export default {
           this.humidity = list.main.humidity
           this.icon = list.weather[0].icon
           this.weather = list.weather[0].main
+           
           
         })
+       
         .catch(weatherError=>{
             console.log(weatherError)
         })
+
     
   
+    },
+    selectIcon(){
+      let iconurl = "http://openweathermap.org/img/w/" + this.icon + ".png"
+      this.iconurl = iconurl
     },
     dateBuilder () {
       const d = new Date();
@@ -141,21 +165,55 @@ export default {
 
 
 <style scoped>
+.wee{
+  margin-left:170px
+}
 .gray--text {
   color:rgb(100, 100, 100)
 }
 .v-application .ma-5 {
      margin: 0px  !important; 
 }
-#app{
-  background-image: require("@/assets/picture/cold.jpg");
-  background-size: cover;
-  background-position: bottom;
-  transition: 0.4s;
-}
-#app.warm{
-  background-image: require("@/assets/picture/warm1.jpg");
+.color{
+   background: rgb(248, 246, 246);
+   font-family: 'Poiret One', cursive;
+   margin: 5% 5% 5% 5%;
 }
 
+.weather{
+  
+  background: rgb(248, 246, 246);
+  font-family: 'Poiret One', cursive;
+}
+
+.sun {
+  animation: rotate 12s infinite linear; 
+  position: relative;
+  display: inline-block;
+  width: 7em;
+  height: 7em;
+  font-size: 1em;
+
+}.nosun{
+  position: relative;
+  display: inline-block;
+  width: 7em;
+  height: 7em;
+  font-size: 1em;
+
+}
+
+@keyframes rotate {
+  from {
+    -webkit-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  to {
+    -webkit-transform: rotate(360deg);
+    -o-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
 
 </style>
